f3b38e7f7f372f41ccf1cc40ce826521ef9d11bb
fengkeyin661-wq <fengkeyin661@gmail.com>
2025-12-01 23:21:10 +0800

    feat: Remove mobile patient app functionality
    The `MobilePatientApp` component and its associated service functions and routing have been removed. This simplifies the application by removing unused features and dependencies.

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ToastProvider } from './components/Toast';
 
@@ -13,10 +12,8 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
-    </BrowserRouter>
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </React.StrictMode>
 );
